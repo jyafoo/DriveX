48b981e230f63533f9369bdc19ff04802e60a137
GIMME <3086660647@qq.com>
2024-08-23 21:15:00 +0800

    refactor: 引入redisson分布式锁优化司机抢单功能

--- service/service-order/src/main/java/com/atguigu/daijia/order/service/impl/OrderInfoServiceImpl.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/service/impl/OrderInfoServiceImpl.java
@@ -13,6 +13,8 @@ import com.atguigu.daijia.order.service.OrderInfoService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -32,24 +34,28 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
     private final OrderInfoMapper orderInfoMapper;
     private final OrderStatusLogMapper orderStatusLogMapper;
     private final RedisTemplate redisTemplate;
+    private final RedissonClient redissonClient;
 
     @Transactional(rollbackFor = {Exception.class})
     @Override
     public Long saveOrderInfo(OrderInfoForm orderInfoForm) {
         OrderInfo orderInfo = new OrderInfo();
         BeanUtils.copyProperties(orderInfoForm, orderInfo);
-        String orderNo = UUID.randomUUID().toString().replaceAll("-",""); // 生成订单id
+        String orderNo = UUID.randomUUID().toString().replaceAll("-", ""); // 生成订单id
         orderInfo.setStatus(OrderStatus.WAITING_ACCEPT.getStatus());
         orderInfo.setOrderNo(orderNo);
         orderInfoMapper.insert(orderInfo);
 
-        //记录日志
+        // 记录日志
         this.log(orderInfo.getId(), orderInfo.getStatus());
 
         // TODO (JIA,2024/8/22,21:09) 亮点四：司机抢单（1、乘客下单那将订单标识先保存至redis）
-        //接单标识，标识不存在了说明不在等待接单状态了
-        // TODO (JIA,2024/8/22,21:23) key不用订单id？？？？？？？
-        redisTemplate.opsForValue().set(RedisConstant.ORDER_ACCEPT_MARK  + orderInfo.getId(), "0", RedisConstant.ORDER_ACCEPT_MARK_EXPIRES_TIME, TimeUnit.MINUTES);
+        // 接单标识，标识不存在了说明不在等待接单状态了
+        redisTemplate.opsForValue().set(
+                RedisConstant.ORDER_ACCEPT_MARK + orderInfo.getId(),  // TODO (JIA,2024/8/22,21:23) key不用订单id？？？？？？？
+                "0",
+                RedisConstant.ORDER_ACCEPT_MARK_EXPIRES_TIME,
+                TimeUnit.MINUTES);
         return orderInfo.getId();
     }
 
@@ -60,8 +66,8 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
         queryWrapper.eq(OrderInfo::getId, orderId);
         queryWrapper.select(OrderInfo::getStatus);
         OrderInfo orderInfo = orderInfoMapper.selectOne(queryWrapper);
-        if(null == orderInfo) {
-            //返回null，feign解析会抛出异常，给默认值，后续会用
+        if (null == orderInfo) {
+            // 返回null，feign解析会抛出异常，给默认值，后续会用
             return OrderStatus.NULL_ORDER.getStatus();
         }
         return orderInfo.getStatus();
@@ -83,31 +89,62 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
     public Boolean robNewOrder(Long driverId, Long orderId) {
         // TODO (JIA,2024/8/22,21:09) 亮点四：司机抢单（2、司机通过redis获取订单信息并抢单且删除订单标识）
         // 抢单成功或取消订单，都会删除该key，redis判断，减少数据库压力
-        // TODO (JIA,2024/8/22,21:23) key不用订单id？？？？？？？
-        if (Boolean.FALSE.equals(redisTemplate.hasKey(RedisConstant.ORDER_ACCEPT_MARK + orderId))) {
+
+        if (Boolean.FALSE.equals(redisTemplate.hasKey(RedisConstant.ORDER_ACCEPT_MARK + orderId))) { // TODO (JIA,2024/8/22,21:23) key不用订单id？？？？？？？
             throw new GuiguException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
         }
 
-        //修改订单状态及司机id
-        //update order_info set status = 2, driver_id = #{driverId}, accept_time = now() where id = #{id}
-        //修改字段
-        OrderInfo orderInfo = new OrderInfo();
-        orderInfo.setId(orderId);
-        orderInfo.setStatus(OrderStatus.ACCEPTED.getStatus());
-        orderInfo.setAcceptTime(new Date());
-        orderInfo.setDriverId(driverId);
-        int rows = orderInfoMapper.updateById(orderInfo);
-        if(rows != 1) {
-            //抢单失败
+        // 创建锁
+        RLock lock = redissonClient.getLock(RedisConstant.ROB_NEW_ORDER_LOCK + orderId);
+
+        // 获取锁
+        try {
+            boolean flag = lock.tryLock(
+                    RedisConstant.ROB_NEW_ORDER_LOCK_WAIT_TIME,
+                    RedisConstant.ROB_NEW_ORDER_LOCK_LEASE_TIME,
+                    TimeUnit.SECONDS
+            );
+
+            if (flag) {
+                // 做一个dcl
+                if (Boolean.FALSE.equals(redisTemplate.hasKey(RedisConstant.ORDER_ACCEPT_MARK + orderId))) { // TODO (JIA,2024/8/22,21:23) key不用订单id？？？？？？？
+                    throw new GuiguException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+                }
+
+                // 修改订单状态及司机id
+                // update order_info set status = 2, driver_id = #{driverId}, accept_time = now() where id = #{id} and status = 1
+                LambdaQueryWrapper<OrderInfo> wrapper = new LambdaQueryWrapper<OrderInfo>()
+                        .eq(OrderInfo::getId, orderId)
+                        .eq(OrderInfo::getStatus, OrderStatus.WAITING_ACCEPT);
+
+                // 修改字段
+                OrderInfo orderInfo = new OrderInfo();
+                orderInfo.setId(orderId);
+                orderInfo.setStatus(OrderStatus.ACCEPTED.getStatus());
+                orderInfo.setAcceptTime(new Date());
+                orderInfo.setDriverId(driverId);
+
+                int rows = orderInfoMapper.update(orderInfo, wrapper);
+                if (rows != 1) {
+                    // 抢单失败
+                    throw new GuiguException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+                }
+
+                // 记录日志
+                this.log(orderId, orderInfo.getStatus());
+
+                // 删除redis订单标识
+                redisTemplate.delete(RedisConstant.ORDER_ACCEPT_MARK);
+            }
+        } catch (InterruptedException e) {
+            // 抢单失败
             throw new GuiguException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+        } finally {
+            // 释放
+            if (lock.isLocked()) {
+                lock.unlock();
+            }
         }
-
-        //记录日志
-        this.log(orderId, orderInfo.getStatus());
-
-        //删除redis订单标识
-        redisTemplate.delete(RedisConstant.ORDER_ACCEPT_MARK);
-
         return true;
     }
 
