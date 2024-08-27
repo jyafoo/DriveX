b49374d34443a24ec19945c5a715adfceeebd688
GIMME <3086660647@qq.com>
2024-08-27 20:35:32 +0800

    feat: 基于minio上传接口完成保存订单监控记录数据功能

--- service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderMonitorController.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderMonitorController.java
@@ -1,15 +1,35 @@
 package com.atguigu.daijia.order.controller;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
 import com.atguigu.daijia.order.service.OrderMonitorService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order/monitor")
 @SuppressWarnings({"unchecked", "rawtypes"})
+@RequiredArgsConstructor
 public class OrderMonitorController {
 
+    private final OrderMonitorService orderMonitorService;
+
+    /**
+     * 保存订单监控记录数据
+     *
+     * @param orderMonitorRecord 订单监控记录对象，包含要保存的订单监控数据
+     * @return 布尔值，表示保存操作是否成功
+     */
+    @Operation(summary = "保存订单监控记录数据")
+    @PostMapping("/saveOrderMonitorRecord")
+    public Result<Boolean> saveMonitorRecord(@RequestBody OrderMonitorRecord orderMonitorRecord) {
+        return Result.ok(orderMonitorService.saveOrderMonitorRecord(orderMonitorRecord));
+    }
 
 }
 
--- service/service-order/src/main/java/com/atguigu/daijia/order/service/OrderMonitorService.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/service/OrderMonitorService.java
@@ -1,8 +1,16 @@
 package com.atguigu.daijia.order.service;
 
 import com.atguigu.daijia.model.entity.order.OrderMonitor;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface OrderMonitorService extends IService<OrderMonitor> {
 
+    /**
+     * 保存订单监控记录数据
+     *
+     * @param orderMonitorRecord 订单监控记录对象，包含要保存的订单监控数据
+     * @return 布尔值，表示保存操作是否成功
+     */
+    Boolean saveOrderMonitorRecord(OrderMonitorRecord orderMonitorRecord);
 }
