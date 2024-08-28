6b524ea6b03db8e3cb0a10d2662ece073880403c
GIMME <3086660647@qq.com>
2024-08-28 11:31:39 +0800

    feat: 通过Drools规则引擎完成计算司机接单奖励功能

--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/RewardRuleService.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/RewardRuleService.java
@@ -1,5 +1,15 @@
 package com.atguigu.daijia.rules.service;
 
+import com.atguigu.daijia.model.form.rules.RewardRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.RewardRuleResponseVo;
+
 public interface RewardRuleService {
 
+    /**
+     * 计算订单奖励费用
+     *
+     * @param rewardRuleRequestForm 奖励规则请求表单，包含计算奖励费用所需的所有参数
+     * @return 奖励费用等信息
+     */
+    RewardRuleResponseVo calculateOrderRewardFee(RewardRuleRequestForm rewardRuleRequestForm);
 }
