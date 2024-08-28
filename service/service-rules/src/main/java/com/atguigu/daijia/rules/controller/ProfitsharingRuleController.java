ae86e8c9f0de6fca821da09c86050f1e58d007da
GIMME <3086660647@qq.com>
2024-08-28 14:28:16 +0800

    feat: 通过Drools规则引擎完成司机端计算分账信息功能

--- service/service-rules/src/main/java/com/atguigu/daijia/rules/controller/ProfitsharingRuleController.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/controller/ProfitsharingRuleController.java
@@ -1,8 +1,15 @@
 package com.atguigu.daijia.rules.controller;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.form.rules.ProfitsharingRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.ProfitsharingRuleResponseVo;
 import com.atguigu.daijia.rules.service.ProfitsharingRuleService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rules/profitsharing")
 @SuppressWarnings({"unchecked", "rawtypes"})
+@RequiredArgsConstructor
 public class ProfitsharingRuleController {
 
+    private ProfitsharingRuleService profitsharingRuleService;
+
+    /**
+     * 计算系统分账费用
+     *
+     * @param profitsharingRuleRequestForm 分账规则请求表，包含订单金额及分账规则等必要信息
+     * @return 各分账方的费用分配详情
+     */
+    @Operation(summary = "计算系统分账费用")
+    @PostMapping("/calculateOrderProfitsharingFee")
+    public Result<ProfitsharingRuleResponseVo> calculateOrderProfitsharingFee(@RequestBody ProfitsharingRuleRequestForm profitsharingRuleRequestForm) {
+        return Result.ok(profitsharingRuleService.calculateOrderProfitsharingFee(profitsharingRuleRequestForm));
+    }
 
 }
 
--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/ProfitsharingRuleService.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/ProfitsharingRuleService.java
@@ -1,5 +1,15 @@
 package com.atguigu.daijia.rules.service;
 
+import com.atguigu.daijia.model.form.rules.ProfitsharingRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.ProfitsharingRuleResponseVo;
+
 public interface ProfitsharingRuleService {
 
+    /**
+     * 计算系统分账费用
+     *
+     * @param profitsharingRuleRequestForm 分账规则请求表，包含订单金额及分账规则等必要信息
+     * @return 各分账方的费用分配详情
+     */
+    ProfitsharingRuleResponseVo calculateOrderProfitsharingFee(ProfitsharingRuleRequestForm profitsharingRuleRequestForm);
 }
