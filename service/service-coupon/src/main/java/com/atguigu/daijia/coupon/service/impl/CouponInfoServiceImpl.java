d601213e44281d1934188e476d49c418b47d278e
GIMME <3086660647@qq.com>
2024-09-01 11:07:14 +0800

    refactor: 优化乘客web端发起支付时使用优惠券功能

--- service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
+++ service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
@@ -65,19 +65,19 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
     @Override
     public Boolean receive(Long customerId, Long couponId) {
 
-        //1、查询优惠券
+        // 1、查询优惠券
         CouponInfo couponInfo = this.getById(couponId);
-        if(null == couponInfo) {
+        if (null == couponInfo) {
             throw new GuiguException(ResultCodeEnum.DATA_ERROR);
         }
 
-        //2、优惠券过期日期判断
+        // 2、优惠券过期日期判断
         if (couponInfo.getExpireTime().before(new Date())) {
             throw new GuiguException(ResultCodeEnum.COUPON_EXPIRE);
         }
 
-        //3、校验库存，优惠券领取数量判断
-        if (couponInfo.getPublishCount() !=0 && couponInfo.getReceiveCount() >= couponInfo.getPublishCount()) {
+        // 3、校验库存，优惠券领取数量判断
+        if (couponInfo.getPublishCount() != 0 && couponInfo.getReceiveCount() >= couponInfo.getPublishCount()) {
             throw new GuiguException(ResultCodeEnum.COUPON_LESS);
         }
 
@@ -103,29 +103,29 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
         RLock lock = null;
         try {
             // 初始化分布式锁
-            //每人领取限制  与 优惠券发行总数 必须保证原子性，使用customerId减少锁的粒度，增加并发能力
+            // 每人领取限制  与 优惠券发行总数 必须保证原子性，使用customerId减少锁的粒度，增加并发能力
             lock = redissonClient.getLock(RedisConstant.COUPON_LOCK + customerId);
             boolean flag = lock.tryLock(RedisConstant.COUPON_LOCK_WAIT_TIME, RedisConstant.COUPON_LOCK_LEASE_TIME, TimeUnit.SECONDS);
             if (flag) {
-                //4、校验每人限领数量
+                // 4、校验每人限领数量
                 if (couponInfo.getPerLimit() > 0) {
-                    //4.1、统计当前用户对当前优惠券的已经领取的数量
+                    // 4.1、统计当前用户对当前优惠券的已经领取的数量
                     long count = customerCouponMapper.selectCount(new LambdaQueryWrapper<CustomerCoupon>().eq(CustomerCoupon::getCouponId, couponId).eq(CustomerCoupon::getCustomerId, customerId));
-                    //4.2、校验限领数量
+                    // 4.2、校验限领数量
                     if (count >= couponInfo.getPerLimit()) {
                         throw new GuiguException(ResultCodeEnum.COUPON_USER_LIMIT);
                     }
                 }
 
-                //5、更新优惠券领取数量
+                // 5、更新优惠券领取数量
                 int row = 0;
-                if (couponInfo.getPublishCount() == 0) {//没有限制
+                if (couponInfo.getPublishCount() == 0) {// 没有限制
                     row = couponInfoMapper.updateReceiveCount(couponId);
                 } else {
                     row = couponInfoMapper.updateReceiveCountByLimit(couponId);
                 }
                 if (row == 1) {
-                    //6、保存领取记录
+                    // 6、保存领取记录
                     this.saveCustomerCoupon(customerId, couponId, couponInfo.getExpireTime());
                     return true;
                 }
@@ -144,9 +144,9 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
      * 保存用户优惠券信息
      *
      * @param customerId 用户ID，用于关联用户信息
-     * @param couponId 优惠券ID，标识优惠券的类型或来源
+     * @param couponId   优惠券ID，标识优惠券的类型或来源
      * @param expireTime 过期时间，标记优惠券的有效期限
-     * 此方法用于创建用户领取优惠券的记录，包括关联用户、优惠券信息，并设置初始状态为有效，记录领取时间及过期时间
+     *                   此方法用于创建用户领取优惠券的记录，包括关联用户、优惠券信息，并设置初始状态为有效，记录领取时间及过期时间
      */
     private void saveCustomerCoupon(Long customerId, Long couponId, Date expireTime) {
         // 创建用户优惠券实体对象
@@ -168,55 +168,50 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
 
     @Override
     public List<AvailableCouponVo> findAvailableCoupon(Long customerId, BigDecimal orderAmount) {
-        // TODO (JIA,2024/8/31,21:52) 待看1
-        //1.定义符合条件的优惠券信息容器
+        // 1.定义符合条件的优惠券信息容器
         List<AvailableCouponVo> availableCouponVoList = new ArrayList<>();
 
-        //2.获取未使用的优惠券列表
+        // 2.获取未使用的优惠券列表
         List<NoUseCouponVo> list = couponInfoMapper.findNoUseList(customerId);
-        //2.1.现金券
-        List<NoUseCouponVo> type1List = list.stream().filter(item -> item.getCouponType().intValue() == 1).collect(Collectors.toList());
+
+        // 2.1.现金券
+        List<NoUseCouponVo> type1List = list.stream().filter(item -> item.getCouponType().intValue() == 1).toList();
         for (NoUseCouponVo noUseCouponVo : type1List) {
-            //使用门槛判断
-            //2.1.1.没门槛，订单金额必须大于优惠券减免金额
-            //减免金额
+            // 使用门槛判断
+            // 2.1.1.没门槛，订单金额必须大于优惠券减免金额
+            // 减免金额
             BigDecimal reduceAmount = noUseCouponVo.getAmount();
             if (noUseCouponVo.getConditionAmount().doubleValue() == 0 && orderAmount.subtract(reduceAmount).doubleValue() > 0) {
                 availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo, reduceAmount));
             }
-            //2.1.2.有门槛，订单金额大于优惠券门槛金额
+            // 2.1.2.有门槛，订单金额大于优惠券门槛金额
             if (noUseCouponVo.getConditionAmount().doubleValue() > 0 && orderAmount.subtract(noUseCouponVo.getConditionAmount()).doubleValue() > 0) {
                 availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo, reduceAmount));
             }
         }
 
-        //2.2.折扣券
-        List<NoUseCouponVo> type2List = list.stream().filter(item -> item.getCouponType().intValue() == 2).collect(Collectors.toList());
+        // 2.2.折扣券
+        List<NoUseCouponVo> type2List = list.stream().filter(item -> item.getCouponType().intValue() == 2).toList();
         for (NoUseCouponVo noUseCouponVo : type2List) {
-            //使用门槛判断
-            //订单折扣后金额
+            // 使用门槛判断
+            // 订单折扣后金额
             BigDecimal discountOrderAmount = orderAmount.multiply(noUseCouponVo.getDiscount()).divide(new BigDecimal("10")).setScale(2, RoundingMode.HALF_UP);
-            //减免金额
+            // 减免金额
             BigDecimal reduceAmount = orderAmount.subtract(discountOrderAmount);
-            //订单优惠金额
-            //2.2.1.没门槛
+            // 订单优惠金额
+            // 2.2.1.没门槛，订单金额大于优惠门槛
             if (noUseCouponVo.getConditionAmount().doubleValue() == 0) {
                 availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo, reduceAmount));
             }
-            //2.2.2.有门槛，订单折扣后金额大于优惠券门槛金额
+            // 2.2.2.有门槛，订单折扣后金额大于优惠券门槛金额
             if (noUseCouponVo.getConditionAmount().doubleValue() > 0 && discountOrderAmount.subtract(noUseCouponVo.getConditionAmount()).doubleValue() > 0) {
                 availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo, reduceAmount));
             }
         }
 
-        //排序
+        // 排序
         if (!CollectionUtils.isEmpty(availableCouponVoList)) {
-            Collections.sort(availableCouponVoList, new Comparator<AvailableCouponVo>() {
-                @Override
-                public int compare(AvailableCouponVo o1, AvailableCouponVo o2) {
-                    return o1.getReduceAmount().compareTo(o2.getReduceAmount());
-                }
-            });
+            availableCouponVoList.sort(Comparator.comparing(AvailableCouponVo::getReduceAmount));
         }
         return availableCouponVoList;
     }
@@ -226,7 +221,7 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
      * 此方法用于将选定的未使用优惠券信息转换为可用优惠券信息对象，并设置优惠券ID和减免金额
      *
      * @param noUseCouponVo 未使用的优惠券信息
-     * @param reduceAmount 减免的金额，表示使用优惠券后减少的消费额
+     * @param reduceAmount  减免的金额，表示使用优惠券后减少的消费额
      * @return 返回转换后的最佳未使用优惠券信息对象
      */
     private AvailableCouponVo buildBestNoUseCouponVo(NoUseCouponVo noUseCouponVo, BigDecimal reduceAmount) {
@@ -245,53 +240,54 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
     @Transactional(noRollbackFor = Exception.class)
     @Override
     public BigDecimal useCoupon(UseCouponForm useCouponForm) {
-        //获取乘客优惠券
+        // 获取乘客优惠券
         CustomerCoupon customerCoupon = customerCouponMapper.selectById(useCouponForm.getCustomerCouponId());
-        if(null == customerCoupon) {
+        if (null == customerCoupon) {
             throw new GuiguException(ResultCodeEnum.ARGUMENT_VALID_ERROR);
         }
-        //获取优惠券信息
+        // 获取优惠券信息
         CouponInfo couponInfo = couponInfoMapper.selectById(customerCoupon.getCouponId());
-        if(null == couponInfo) {
+        if (null == couponInfo) {
             throw new GuiguException(ResultCodeEnum.ARGUMENT_VALID_ERROR);
         }
-        //判断该优惠券是否为乘客所有
-        if(customerCoupon.getCustomerId().longValue() != useCouponForm.getCustomerId().longValue()) {
+        // 判断该优惠券是否为乘客所有
+        if (customerCoupon.getCustomerId().longValue() != useCouponForm.getCustomerId().longValue()) {
             throw new GuiguException(ResultCodeEnum.ILLEGAL_REQUEST);
         }
-        //获取优惠券减免金额
+        // 获取优惠券减免金额
         BigDecimal reduceAmount = null;
-        if(couponInfo.getCouponType().intValue() == 1) {
-            //使用门槛判断
-            //2.1.1.没门槛，订单金额必须大于优惠券减免金额
+        if (couponInfo.getCouponType().intValue() == 1) {
+            // 使用门槛判断
+            // 2.1.1.没门槛，订单金额必须大于优惠券减免金额
             if (couponInfo.getConditionAmount().doubleValue() == 0 && useCouponForm.getOrderAmount().subtract(couponInfo.getAmount()).doubleValue() > 0) {
-                //减免金额
+                // 减免金额
                 reduceAmount = couponInfo.getAmount();
             }
-            //2.1.2.有门槛，订单金额大于优惠券门槛金额
+            // 2.1.2.有门槛，订单金额大于优惠券门槛金额
             if (couponInfo.getConditionAmount().doubleValue() > 0 && useCouponForm.getOrderAmount().subtract(couponInfo.getConditionAmount()).doubleValue() > 0) {
-                //减免金额
+                // 减免金额
                 reduceAmount = couponInfo.getAmount();
             }
         } else {
-            //使用门槛判断
-            //订单折扣后金额
+            // 使用门槛判断
+            // 订单折扣后金额
             BigDecimal discountOrderAmount = useCouponForm.getOrderAmount().multiply(couponInfo.getDiscount()).divide(new BigDecimal("10")).setScale(2, RoundingMode.HALF_UP);
-            //订单优惠金额
-            //2.2.1.没门槛
+            // 订单优惠金额
+            // 2.2.1.没门槛
             if (couponInfo.getConditionAmount().doubleValue() == 0) {
-                //减免金额
+                // 减免金额
                 reduceAmount = useCouponForm.getOrderAmount().subtract(discountOrderAmount);
             }
-            //2.2.2.有门槛，订单折扣后金额大于优惠券门槛金额
+            // 2.2.2.有门槛，订单折扣后金额大于优惠券门槛金额
             if (couponInfo.getConditionAmount().doubleValue() > 0 && discountOrderAmount.subtract(couponInfo.getConditionAmount()).doubleValue() > 0) {
-                //减免金额
+                // 减免金额
                 reduceAmount = useCouponForm.getOrderAmount().subtract(discountOrderAmount);
             }
         }
-        if(reduceAmount.doubleValue() > 0) {
+
+        if (reduceAmount.doubleValue() > 0) {
             int row = couponInfoMapper.updateUseCount(couponInfo.getId());
-            if(row == 1) {
+            if (row == 1) {
                 CustomerCoupon updateCustomerCoupon = new CustomerCoupon();
                 updateCustomerCoupon.setId(customerCoupon.getId());
                 updateCustomerCoupon.setUsedTime(new Date());
@@ -302,5 +298,4 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
         }
         throw new GuiguException(ResultCodeEnum.DATA_ERROR);
     }
-
 }
--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.atguigu.daijia.customer.service.impl;
 
 import com.atguigu.daijia.common.execption.GuiguException;
 import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.coupon.client.CouponFeignClient;
 import com.atguigu.daijia.customer.client.CustomerInfoFeignClient;
 import com.atguigu.daijia.customer.service.OrderService;
 import com.atguigu.daijia.dispatch.client.NewOrderFeignClient;
@@ -11,6 +12,7 @@ import com.atguigu.daijia.map.client.MapFeignClient;
 import com.atguigu.daijia.map.client.WxPayFeignClient;
 import com.atguigu.daijia.model.entity.order.OrderInfo;
 import com.atguigu.daijia.model.enums.OrderStatus;
+import com.atguigu.daijia.model.form.coupon.UseCouponForm;
 import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
 import com.atguigu.daijia.model.form.customer.SubmitOrderForm;
 import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
@@ -33,12 +35,14 @@ import com.atguigu.daijia.model.vo.payment.WxPrepayVo;
 import com.atguigu.daijia.model.vo.rules.FeeRuleResponseVo;
 import com.atguigu.daijia.order.client.OrderInfoFeignClient;
 import com.atguigu.daijia.rules.client.FeeRuleFeignClient;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Slf4j
@@ -55,6 +59,7 @@ public class OrderServiceImpl implements OrderService {
     private final LocationFeignClient locationFeignClient;
     private final CustomerInfoFeignClient customerInfoFeignClient;
     private final WxPayFeignClient wxPayFeignClient;
+    private final CouponFeignClient couponFeignClient;
 
     @Override
     public ExpectOrderVo expectOrder(ExpectOrderForm expectOrderForm) {
@@ -187,6 +192,7 @@ public class OrderServiceImpl implements OrderService {
         return orderInfoFeignClient.findCustomerOrderPage(customerId, page, limit).getData();
     }
 
+    @GlobalTransactional
     @Override
     public WxPrepayVo createWxPayment(CreateWxPaymentForm createWxPaymentForm) {
         //1.获取订单支付相关信息
@@ -202,16 +208,39 @@ public class OrderServiceImpl implements OrderService {
         //3.获取司机微信openId
         String driverOpenId = driverInfoFeignClient.getDriverOpenId(orderPayVo.getDriverId()).getData();
 
-        //4.封装微信下单对象，微信支付只关注以下订单属性
+        //4.处理优惠券
+        BigDecimal couponAmount = null;
+        //支付时选择过一次优惠券，如果支付失败或未支付，下次支付时不能再次选择，只能使用第一次选中的优惠券（前端已控制，后端再次校验）
+        if (null == orderPayVo.getCouponAmount() && null != createWxPaymentForm.getCustomerCouponId() && createWxPaymentForm.getCustomerCouponId() != 0) {
+            UseCouponForm useCouponForm = new UseCouponForm();
+            useCouponForm.setOrderId(orderPayVo.getOrderId());
+            useCouponForm.setCustomerCouponId(createWxPaymentForm.getCustomerCouponId());
+            useCouponForm.setOrderAmount(orderPayVo.getPayAmount());
+            useCouponForm.setCustomerId(createWxPaymentForm.getCustomerId());
+            couponAmount = couponFeignClient.useCoupon(useCouponForm).getData();
+        }
+
+        //5.更新账单优惠券金额
+        //支付金额
+        BigDecimal payAmount = orderPayVo.getPayAmount();
+        if (null != couponAmount) {
+            Boolean isUpdate = orderInfoFeignClient.updateCouponAmount(orderPayVo.getOrderId(), couponAmount).getData();
+            if (Boolean.FALSE.equals(isUpdate)) {
+                throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+            }
+            //当前支付金额 = 支付金额 - 优惠券金额
+            payAmount = payAmount.subtract(couponAmount);
+        }
+
+        //6.封装微信下单对象，微信支付只关注以下订单属性
         PaymentInfoForm paymentInfoForm = new PaymentInfoForm();
         paymentInfoForm.setCustomerOpenId(customerOpenId);
         paymentInfoForm.setDriverOpenId(driverOpenId);
         paymentInfoForm.setOrderNo(orderPayVo.getOrderNo());
-        paymentInfoForm.setAmount(orderPayVo.getPayAmount());
+        paymentInfoForm.setAmount(payAmount);
         paymentInfoForm.setContent(orderPayVo.getContent());
         paymentInfoForm.setPayWay(1);
-        WxPrepayVo wxPrepayVo = wxPayFeignClient.createWxPayment(paymentInfoForm).getData();
-        return wxPrepayVo;
+        return wxPayFeignClient.createWxPayment(paymentInfoForm).getData();
     }
 
     @Override
